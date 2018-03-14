6b3f04ae9df197e24b47ba348ee4566ff7b1cac5
dabbar <xzanich@gmail.com>
2018-03-14 14:51:05 +0300

    INIT.

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,27 +1,3 @@
-(function () {
-    document.write('YO TSawdaw!');
-
-    const longLine: string = `lorem awdaw dawd awd awdawdawdawd
-     awdaw dawd awda wdawd awd awdaw dawd awd awd awd awd awd
-     awdaw dawdaw dawdaw dawd awd awda wdaw  awd awdawdawd
-     awd awdawdawdawdawd awd awdaw dawd awdaw dawd awd awdaw daw d`;
-
-    document.write(longLine);
-
-    console.warn('Yey', 'aw');
-    console.warn('just testing:');
-
-    const arr = [1, 2, 3, 4, 5];
-
-    for (const item of arr) {
-        document.write(item.toString());
-    }
-
-    const prom = Promise.resolve(1);
-
-    async function test() {
-        console.warn('await prom', await prom);
-    }
-
-    test();
+(function() {
+    console.warn('Works');
 })();
